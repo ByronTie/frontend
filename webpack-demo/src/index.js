b6d2bdd723c505058566a6b5049199dc0fa382bb
Unknown <offlimits118@yahoo.com>
2018-07-31 23:03:39 -0700

    Webpack-demo: asset management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,8 +1,19 @@
 import _ from 'lodash';
+import '../styles/style.css';
+import favIcon from '../images/favIcon.ico';
+import Data from '../data/data.xml';
 
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'world'], ' ');
+    element.classList.add("hello");
+
+    var myIcon = new Image();
+    myIcon.src = favIcon;
+
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 };
