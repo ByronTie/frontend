dee86a2a65048bf56d948b4155b4ea7b4f1000d9
Unknown <offlimits118@yahoo.com>
2018-08-02 16:14:50 -0700

    Webpack-demo: output management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,19 +2,24 @@ import _ from 'lodash';
 import '../styles/style.css';
 import favIcon from '../images/favIcon.ico';
 import Data from '../data/data.xml';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var myIcon = new Image();
+    var btn = document.createElement('button');
+
     element.innerHTML = _.join(['Hello', 'world'], ' ');
     element.classList.add("hello");
 
-    var myIcon = new Image();
     myIcon.src = favIcon;
-
     element.appendChild(myIcon);
 
-    console.log(Data);
+    btn.innerHTML = "Click me and check the console";
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
+    console.log(Data);
     return element;
 };
 
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        another: './src/another-module.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin([
+            'dist'
+        ]),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     module: {
         rules: [
